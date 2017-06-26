25dea6da0cbd63ef8e91d3dcbdcc245c8ef2fcf0
drollo1 <drollo1@umbc.edu>
2017-06-25 21:43:47 -0400

    Update push

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -1,41 +1,66 @@
 #include "Interpreter.h"
 
 Interpreter::Interpreter(){
-
 }
+
 Interpreter::Interpreter(string cmd_name, string data_name ){
+	
+	parse_instructions(cmd_name);
+	run();
+	print_code();
+} 
+
+void Interpreter::parse_instructions(string cmd_name){
+
 	num_line=0;
 	string line;
 	ifstream cmd_file;
 	cmd_file.open(cmd_name.c_str());
-
 	while(getline(cmd_file, line)){
-
 		string token="";
 		int inst_token=0;
-		int op_token=0;
-		for(int i=0;i<line.length()&&!op_token;i++){
-			if (line[i]!=' '){
+		for(int i=0;i<line.length();i++){
+			if (line[i]!=' '&&line[i]!='	'){
 				token+=line[i];
 				if (line[i]==':'){
 					cmd_lines[num_line].header=token;
 					token="";
 				}
-				if(inst_token){
-					cmd_lines[num_line].operation=line.substr(i,line.length());
-					op_token=1;
+				else if((line[i+1]==' '||line[i+1]=='	'||line[i+1]==00)&&!inst_token){
+					cmd_lines[num_line].instruction=token;
+					cmd_lines[num_line].line_numb=num_line;
+					inst_token =1;
+					token="";
+					num_line++;
+					cmd_lines[num_line-1].line_numb = num_line;
 				}
-			}
-			else if(line[i-1]!=' '){
-				cmd_lines[num_line].instruction=token;
-				num_line ++;
-				cmd_lines[num_line].line_numb=num_line;
-				inst_token =1;
-				token="";
-			}
+				else if(inst_token&&line[i]!=00){
+					cmd_lines[num_line-1].operation=line.substr(i,line.length());
+					i=line.length()-1;
+					inst_token=0;
+				}
+			}			
+			
 		}
 	}
+}
 
-	for(int i=0; i< num_line;i++)
-		cout<<cmd_lines[i].header<<"  instruction: "<<cmd_lines[i].instruction<<"  operation: "<<cmd_lines[i].operation<<"\n";
-} 
+void Interpreter::print_code(){
+	for(int i=0; i< num_line;i++){
+		cout<<cmd_lines[i].line_numb <<"# ";
+		cout<<cmd_lines[i].header<<"  instruction: ";
+		cout<<cmd_lines[i].instruction<<"  operation: ";
+		cout<<cmd_lines[i].operation<<"\n";
+	}
+}
+
+void Interpreter::run(){
+	IF =&cmd_lines[0];
+	while(IF->get_number()!=num_line){
+		int temp=IF->get_number();
+		cout << temp << "\n";
+		IF=&cmd_lines[temp];
+
+
+	}
+}
--- Interpreter.h
+++ Interpreter.h
@@ -11,13 +11,31 @@ class Interpreter{
 struct cmd_line{
 	string header, instruction, operation;
 	int line_numb;
+	string get_header(){return header;}
+	string get_instruction(){return instruction;}
+	string get_operation(){return operation;}
+	int get_number(){return line_numb;}
 };
 
 //cmd_line* program_code = new cmd_line[32];
 	public:
 		Interpreter();
 		Interpreter(string cmd_name, string data_name);
+		void parse_instructions(string cmd_name);
+		void print_code();
+		void run();
+		int if_control();
+		int id_control();
+		int ex1_control();
+		int ex2_control();
+		int ex3_control();
+		int mem_control();
+		int wb_control();
+
 		cmd_line cmd_lines[32];
 		int num_line;
+		int registers[32];
+
+		cmd_line * IF, * ID, * EX1, * EX2, * EX3, * MEM, * WB;
 };
 #endif
